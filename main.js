40f084b629269c2720caa93ce1701671d7964ff9
Hernán Casanova <hernan.casanova.silva@outlook.com>
2023-10-31 21:47:45 -0300

    Cambio de nombre a contenedores de la aplicación

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const routes = require('./src/routes/routes')
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://mongo/prices')
+mongoose.connect('mongodb://mongo-vpf/prices')
   .then(db => console.log('db connected'))
   .catch(err => console.log(err));
 app.use(cors())
